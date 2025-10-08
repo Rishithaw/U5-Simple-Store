ada90e06722b12598c2e7ec1d5b0cf86512046ae
rishithaw <rishithaw1@gmail.com>
2025-10-08 11:05:26 -0500

    Added associations and csv file

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :showw]
   root "products#index"
 end
