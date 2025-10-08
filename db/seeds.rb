4e31f05ab7bafaeb04e41f288ef1fcc87bb16dbc
rishithaw <rishithaw1@gmail.com>
2025-10-08 15:22:39 -0500

    Updated the seed file to read from the csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require "faker"
+# require "faker"
+
+# Product.destroy_all
+
+# puts "Creating 676 products..."
+
+# 676.times do
+#   Product.create!(
+#     title: Faker::Commerce.unique.product_name,
+#     price: Faker::Commerce.price(range: 0..100.0, as_string: false),
+#     stock_quantity: Faker::Number.between(from: 0, to: 1000)
+#   )
+# end
+
+# puts "Created #{Product.count} products."
+
+require "csv"
 
 Product.destroy_all
+Category.destroy_all
 
-puts "Creating 676 products..."
+csv_file = Rails.root.join("products.csv")
+csv_data = CSV.read(csv_file)
 
-676.times do
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |row|
+  category_name = row["category"]
+  category = Category.find_or_create_by(name: category_name)
+
   Product.create!(
-    title: Faker::Commerce.unique.product_name,
-    price: Faker::Commerce.price(range: 0..100.0, as_string: false),
-    stock_quantity: Faker::Number.between(from: 0, to: 1000)
+    title: row["name"],
+    description: row["description"],
+    price: row["price"].to_f,
+    stock_quantity: row["stock_quantity"].to_i,
+    category: category
   )
 end
 
-puts "Created #{Product.count} products."
+puts "Created #{Product.count} products in #{Category.count} categories."
